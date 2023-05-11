1e8c60b88e95060a34a2343228c529731cbca081
Xenia <linshuyou1030@gmail.com>
2023-05-10 17:57:35 -0700

    Connect basic UI flow together

---
+++ AITeacher/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Button loadReadAloudSceneBtn;
+    public Button loadSilentReadSceneBtn;
+
+    void Start()
+    {
+        loadReadAloudSceneBtn.onClick.AddListener(LoadReadAloudScene);
+        loadSilentReadSceneBtn.onClick.AddListener(LoadSilentReadScene);
+    }
+
+    private void LoadReadAloudScene()
+    {
+        //SceneManager.LoadScene("VirtualTeacher");
+        StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.In, "VirtualTeacher"));
+    }
+
+    private void LoadSilentReadScene()
+    {
+        SceneManager.LoadScene("SilentRead");
+    }
+}
--- AITeacher/Assets/Scripts/UIController.cs
+++ AITeacher/Assets/Scripts/UIController.cs
@@ -6,13 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image BackgroundUI;
+    public List<Sprite> BackgroundSpries = new List<Sprite>();
+    public GameObject ChatCanvas;
+    public GameObject TeacherAvatar;
+
+    private Vector3 canvasPosNormal = new Vector3(0.01f, 0.26f, 1.0f);
+    private Vector3 canvasPosUp = new Vector3(0.01f, 0.63f, 1.0f);
+    private RectTransform chatCanvasRT;
+
     void Start()
     {
         if (TriggerController.Instance == null)
         {
             Debug.LogError("TriggerController is null");
         }
+        chatCanvasRT = ChatCanvas.GetComponent<RectTransform>();
+
+        StartCoroutine(EnterWarning());
     }
 
     // Update is called once per frame
@@ -20,42 +31,83 @@ public class UIController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
+            BackgroundUI.sprite = BackgroundSpries[0];
+            chatCanvasRT.localPosition = canvasPosNormal;
             TriggerController.Instance.TriggerActions("warning");
         }
 
         if (Input.GetKeyDown(KeyCode.B))
         {
+            BackgroundUI.sprite = BackgroundSpries[1];
+            chatCanvasRT.localPosition = canvasPosNormal;
             TriggerController.Instance.TriggerActions("start_conversation");
         }
 
         if (Input.GetKeyDown(KeyCode.C))
         {
-            TriggerController.Instance.TriggerActions("good_job");
+            // hide chat
+            ChatCanvas.SetActive(false);
+            TeacherAvatar.SetActive(false);
+            BackgroundUI.sprite = BackgroundSpries[2];
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            TriggerController.Instance.TriggerActions("start_questions");
+            ChatCanvas.SetActive(true);
+            TeacherAvatar.SetActive(true);
+            BackgroundUI.sprite = BackgroundSpries[3];
+            chatCanvasRT.localPosition = canvasPosNormal;
+            TriggerController.Instance.TriggerActions("good_job");
         }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            TriggerController.Instance.TriggerActions("ask_questions");
+            BackgroundUI.sprite = BackgroundSpries[4];
+            chatCanvasRT.localPosition = canvasPosNormal;
+            TriggerController.Instance.TriggerActions("start_questions");
         }
 
         if (Input.GetKeyDown(KeyCode.F))
         {
-            TriggerController.Instance.TriggerActions("vocabularies");
+            BackgroundUI.sprite = BackgroundSpries[5];
+            chatCanvasRT.localPosition = canvasPosNormal;
+            TriggerController.Instance.TriggerActions("ask_questions");
         }
 
         if (Input.GetKeyDown(KeyCode.G))
         {
-            TriggerController.Instance.TriggerActions("give_rewards");
+            BackgroundUI.sprite = BackgroundSpries[6];
+            chatCanvasRT.localPosition = canvasPosNormal;
+            TriggerController.Instance.TriggerActions("ask_questions");
         }
 
         if (Input.GetKeyDown(KeyCode.H))
         {
+            BackgroundUI.sprite = BackgroundSpries[7];
+            chatCanvasRT.localPosition = canvasPosUp; // move chat up to accommodate UI
+            TriggerController.Instance.TriggerActions("vocabularies");
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            BackgroundUI.sprite = BackgroundSpries[8];
+            chatCanvasRT.localPosition = canvasPosNormal;
+            TriggerController.Instance.TriggerActions("give_rewards");
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            chatCanvasRT.localPosition = canvasPosNormal;
             TriggerController.Instance.TriggerActions("share_results"); 
         }
+    }
+
+    IEnumerator EnterWarning()
+    {
+        yield return new WaitForSeconds(1);
+        // Trigger the warning interface before the student starts
+        BackgroundUI.sprite = BackgroundSpries[0];
+        chatCanvasRT.localPosition = canvasPosNormal;
+        TriggerController.Instance.TriggerActions("warning");
     }
 }
