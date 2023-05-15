d0b6c8c3783c9d7918b9d16da04825627a138015
Xenia <linshuyou1030@gmail.com>
2023-05-14 22:58:10 -0700

    Add Fullscreen editor. Add a button to move flow forward for read aloud mode. Fix UI for silent read mode.

--- AITeacher/Assets/Scripts/UIController.cs
+++ AITeacher/Assets/Scripts/UIController.cs
@@ -10,10 +10,12 @@ public class UIController : MonoBehaviour
     public List<Sprite> BackgroundSpries = new List<Sprite>();
     public GameObject ChatCanvas;
     public GameObject TeacherAvatar;
+    public Button NextBtn;
 
     private Vector3 canvasPosNormal = new Vector3(0.01f, 0.26f, 1.0f);
     private Vector3 canvasPosUp = new Vector3(0.01f, 0.63f, 1.0f);
     private RectTransform chatCanvasRT;
+    private int currentFlowIndex = 0;
 
     void Start()
     {
@@ -23,12 +25,12 @@ public class UIController : MonoBehaviour
         }
         chatCanvasRT = ChatCanvas.GetComponent<RectTransform>();
 
-        StartCoroutine(EnterWarning());
+        TriggerNextFlow();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Quickly trigger specific flow for debugging purposes
         if (Input.GetKeyDown(KeyCode.A))
         {
             BackgroundUI.sprite = BackgroundSpries[0];
@@ -78,7 +80,6 @@ public class UIController : MonoBehaviour
         {
             BackgroundUI.sprite = BackgroundSpries[6];
             chatCanvasRT.localPosition = canvasPosNormal;
-            TriggerController.Instance.TriggerActions("ask_questions");
         }
 
         if (Input.GetKeyDown(KeyCode.H))
@@ -100,6 +101,70 @@ public class UIController : MonoBehaviour
             chatCanvasRT.localPosition = canvasPosNormal;
             TriggerController.Instance.TriggerActions("share_results"); 
         }
+    }
+
+    // Enter next flow through button click
+    public void TriggerNextFlow()
+    {
+        Debug.Log(currentFlowIndex);
+        switch(currentFlowIndex)
+        {
+            case(0):
+                StartCoroutine(EnterWarning());
+                break;
+            case(1):
+                BackgroundUI.sprite = BackgroundSpries[1];
+                chatCanvasRT.localPosition = canvasPosNormal;
+                TriggerController.Instance.TriggerActions("start_conversation");
+                break;
+            case(2):
+                // hide chat
+                ChatCanvas.SetActive(false);
+                TeacherAvatar.SetActive(false);
+                BackgroundUI.sprite = BackgroundSpries[2];
+                break;
+            case(3):
+                ChatCanvas.SetActive(true);
+                TeacherAvatar.SetActive(true);
+                BackgroundUI.sprite = BackgroundSpries[3];
+                chatCanvasRT.localPosition = canvasPosNormal;
+                TriggerController.Instance.TriggerActions("good_job");
+                break;
+            case(4):
+                BackgroundUI.sprite = BackgroundSpries[4];
+                chatCanvasRT.localPosition = canvasPosNormal;
+                TriggerController.Instance.TriggerActions("start_questions");
+                break;
+            case(5):
+                BackgroundUI.sprite = BackgroundSpries[5];
+                chatCanvasRT.localPosition = canvasPosNormal;
+                TriggerController.Instance.TriggerActions("ask_questions");
+                break;
+            case(6):
+                BackgroundUI.sprite = BackgroundSpries[6];
+                chatCanvasRT.localPosition = canvasPosNormal;
+                break;
+            case(7):
+                BackgroundUI.sprite = BackgroundSpries[7];
+                chatCanvasRT.localPosition = canvasPosUp; // move chat up to accommodate UI
+                TriggerController.Instance.TriggerActions("vocabularies");
+                break;
+            case(8):
+                BackgroundUI.sprite = BackgroundSpries[8];
+                chatCanvasRT.localPosition = canvasPosNormal;
+                TriggerController.Instance.TriggerActions("give_rewards");
+                break;
+            case(9):
+                chatCanvasRT.localPosition = canvasPosNormal;
+                TriggerController.Instance.TriggerActions("share_results");
+                break;
+            default:
+                break;
+        }
+        if (currentFlowIndex >= 0 && currentFlowIndex < 10)
+            currentFlowIndex += 1;
+        if (currentFlowIndex == 10)
+            currentFlowIndex = 0;
     }
 
     IEnumerator EnterWarning()
