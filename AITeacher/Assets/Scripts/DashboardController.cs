c0fced59d18446b0d14cedf678456fe6a0bcce28
xenialin <linshuyou1030@gmail.com>
2023-05-24 12:43:35 -0700

    Add button to load AR scene from dashboard

--- AITeacher/Assets/Scripts/DashboardController.cs
+++ AITeacher/Assets/Scripts/DashboardController.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public class DashboardController : MonoBehaviour
 {
     public Button loadReadAloudSceneBtn;
     public Button loadSilentReadSceneBtn;
+    public Button loadARSceneBtn;
 
     void Start()
     {
         loadReadAloudSceneBtn.onClick.AddListener(LoadReadAloudScene);
         loadSilentReadSceneBtn.onClick.AddListener(LoadSilentReadScene);
+        loadARSceneBtn.onClick.AddListener(LoadARScene);
     }
 
     private void LoadReadAloudScene()
     {
-        // SceneManager.LoadScene("VirtualTeacher");
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
         StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.In, "VirtualTeacher"));
     }
 
     private void LoadSilentReadScene()
     {
-        // SceneManager.LoadScene("SilentRead");
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
         StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.In, "SilentRead"));
+    }
+
+    private void LoadARScene()
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        //SceneManager.LoadScene("ARMode");
+        StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.In, "ARMode"));
     }
 }
